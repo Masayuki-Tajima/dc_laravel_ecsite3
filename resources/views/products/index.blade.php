6372db183479d8a2d4d92cff7e372e2aecced2bc
Masayuki-Tajima <masatajima.work@gmail.com>
2023-10-19 16:27:28 +0900

    csrf対策追加

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,6 +5,7 @@
         @foreach($products as $product)
         <div class="col-3">
             <form method="post" action="">
+                    @csrf
                     <img src="{{ asset('images/orange.jpg') }}" class="img-thumbnail">
                     <div class="row">
                         <div class="col-12">
