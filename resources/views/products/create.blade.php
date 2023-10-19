7ad5523b06b36f048f713c076bef717a8ca287b6
Masayuki-Tajima <masatajima.work@gmail.com>
2023-10-19 16:44:21 +0900

    products create作成

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container h-100">
+        <div>
+            <h2>商品登録</h2>
+        </div>
+
+        <form action="" method="post" enctype="multipart/form-data">
+            @csrf
+            <div>
+                <label for="product_name">商品名</label>
+                <input type="text" name="product_name" required>
+            </div>
+
+            <div>
+                <label for="price">価格</label>
+                <input type="text" name="price" required>
+            </div>
+
+            <div>
+                <label for="product_qty">個数</label>
+                <input type="text" name="product_qty" required>
+            </div>
+
+            <div>
+                <label for="image">商品画像</label>
+                <input type="file" name="image">
+            </div>
+
+            <div>
+                <label for="public_flag">公開フラグ</label>
+                <select name="public_flag">
+                    <option value="1">公開</option>
+                    <option value="0">非公開</option>
+                </select>
+            </div>
+
+            <input type="submit" value="商品を登録">
+            <a href="{{ route('products.index') }}">戻る</a>
+        </form>
+    </div>
+@endsection
