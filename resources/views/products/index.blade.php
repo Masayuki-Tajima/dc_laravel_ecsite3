6f72e8c59240aa38e0d25bf70122359c0334ab5f
Masayuki-Tajima <masatajima.work@gmail.com>
2023-10-26 13:36:20 +0900

    indexレイアウト整理

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,21 +4,27 @@
     <div class="container h-100">
         @foreach($products as $product)
         <div class="col-3">
+            <img src="{{ asset('images/orange.jpg') }}" class="img-thumbnail">
+            <div class="row">
+                <div class="col-12">
+                    <p>
+                        {{ $product->name }}
+                        {{ $product->price }}円
+                    </p>
+                </div>
+            </div>
+
+            <form method="post" action="{{ route('products.destroy', $product->id) }}">
+                @csrf
+                <input type="submit" name="cartIn" value="カートに入れる">
+            </form>
+
             <form method="post" action="{{ route('products.destroy', $product->id) }}">
                 @csrf
                 @method('DELETE')
-                <img src="{{ asset('images/orange.jpg') }}" class="img-thumbnail">
-                <div class="row">
-                    <div class="col-12">
-                        <p>
-                            {{ $product->name }}
-                            {{ $product->price }}円
-                        </p>
-                    </div>
-                </div>
-                <input type="submit" name="cartIn" value="カートに入れる">
                 <input type="submit" name="delete" value="削除">
             </form>
+
         </div>
         @endforeach
     </div>
