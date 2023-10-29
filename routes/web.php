5586edd0b12fdc9f48605aa342f0067ce2732138
Masayuki-Tajima <masatajima.work@gmail.com>
2023-10-29 22:49:40 +0900

    ログイン状態でない場合、ログイン画面に戻る処理追加

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->middleware('auth');
 
 Route::controller(CartController::class)->group(function () {
     Route::get('users/carts', 'index')->name('carts.index');
